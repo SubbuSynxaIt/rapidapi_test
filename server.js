c0c6f72f472145f7b1162a70576029941a43fd8d
rajiniram5556 <kummithifamily@gmail.com>
2025-06-14 16:44:55 +0530

    quote of the day

---
+++ server.js
@@ -0,0 +1,24 @@
+// server.js
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+const quotes = [
+  { author: "Albert Einstein", quote: "Life is like riding a bicycle. To keep your balance you must keep moving." },
+  { author: "Oscar Wilde", quote: "Be yourself; everyone else is already taken." },
+  { author: "Abraham Lincoln", quote: "Whatever you are, be a good one." },
+  { author: "Confucius", quote: "It does not matter how slowly you go as long as you do not stop." }
+];
+
+// API endpoint
+app.get("/quote", (req, res) => {
+  const random = Math.floor(Math.random() * quotes.length);
+  res.json(quotes[random]);
+});
+
+app.listen(PORT, () => {
+  console.log(`API running at http://localhost:${PORT}`);
+});
