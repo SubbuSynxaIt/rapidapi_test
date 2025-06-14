7ecc43f1d88e920557bee7e515deaeb3ec023ed3
rajiniram5556 <kummithifamily@gmail.com>
2025-06-14 16:57:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,10 @@ const quotes = [
   { author: "Abraham Lincoln", quote: "Whatever you are, be a good one." },
   { author: "Confucius", quote: "It does not matter how slowly you go as long as you do not stop." }
 ];
-
+app.get("/ping", (req, res) => {
+    res.status(200).json({ status: "ok", message: "API is healthy" });
+  });
+  
 // API endpoint
 app.get("/quote", (req, res) => {
   const random = Math.floor(Math.random() * quotes.length);
